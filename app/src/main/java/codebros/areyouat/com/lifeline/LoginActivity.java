92beec6a29aca80e6eb8463c02dc0479eba3bd58
BiOs786 <k.maaz82@gmail.com>
2017-09-05 14:14:25 +0530

    Project Added on 13:36, 05-09-2017

--- app/src/main/java/codebros/areyouat/com/lifeline/LoginActivity.java
+++ app/src/main/java/codebros/areyouat/com/lifeline/LoginActivity.java
@@ -1,7 +1,9 @@
 package codebros.areyouat.com.lifeline;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,4 +14,9 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+
+    public void openHospitalActivity(View view) {
+        Intent intent = new Intent(this, HospitalActivity.class);
+        startActivity(intent);
+    }
 }
